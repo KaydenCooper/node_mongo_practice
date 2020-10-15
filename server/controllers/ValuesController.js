7dbc15353c1428d22e9a1595a7ee59aa8e00ad80
Kayden Cooper <kayden_cooper@yahoo.com>
2020-10-15 12:29:44 -0600

    routes working on postman

--- server/controllers/ValuesController.js
+++ server/controllers/ValuesController.js
@@ -7,20 +7,39 @@ export class ValuesController extends BaseController {
     super("api/values");
     this.router
       .get("", this.getAll)
-      .post("", this.create);
+      .post("", this.create)
+      .put("/:id", this.edit)
+      .delete("/:id", this.delete)
   }
-  async getAll(_, res, next) {
+  async getAll(req, res, next) {
     try {
-      return res.send(["value1", "value2"]);
+      return res.send([await valuesService.find()]);
     } catch (error) {
       next(error);
     }
   }
   async create(req, res, next) {
     try {
-      res.send(req.body);
+      let newValue = await valuesService.create(req.body)
+      res.send(newValue)
     } catch (error) {
       next(error);
+    }
+  }
+  async delete(req, res, next) {
+    try {
+      await valuesService.delete(req.params.id)
+      res.send("Deleted")
+    } catch (error) {
+      next(error)
+    }
+  }
+  async edit(req, res, next) {
+    try {
+      let value = await valuesService.edit(req.params.id, req.body)
+      res.send({ data: value, message: "YOU GOT VALUE" })
+    } catch (error) {
+      next(error)
     }
   }
 }
--- server/services/ValueService.js
+++ server/services/ValueService.js
@@ -2,7 +2,16 @@ import { dbContext } from "../db/DbContext";
 import { BadRequest } from "../utils/Errors";
 
 class ValuesService {
-  async find(query={}) {
+  async create(body) {
+    return await dbContext.Values.create(body)
+  }
+  async edit(id, body) {
+    return await dbContext.Values.findByIdAndUpdate(id, body)
+  }
+  async delete(id) {
+    return await dbContext.Values.findByIdAndDelete(id)
+  }
+  async find(query = {}) {
     let values = await dbContext.Values.find(query);
     return values;
   }
